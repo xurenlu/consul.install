6490e3aa64b29721db57b71fd2be5261ece6db08
黄滚-404.ms <git@404.ms>
2022-09-27 14:24:55 +0800

    --

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo mkdir /opt/consul/
 sudo cp ./etc/consul.* /etc/consul/
 sudo wget -O /usr/bin/consul https://huasheng-resume.oss-cn-beijing.aliyuncs.com/consul
 sudo cp ./conf/consul.conf /etc/supervisor/conf.d/
-IP=`hostname -I|tr " " "\n"|head -n 1`
+export IPIP=`hostname -I|tr " " "\n"|head -n 1`
 sudo sed -i  "s/IPIP/${IPIP}/g" /etc/supervisor/conf.d/consul.conf
 sudo mkdir /var/log/consul/
